f080264645562001eccc959dbdb71371cfd178aa
Yelto Devos <yelto.devos@student.howest.be>
2026-06-06 17:16:15 +0200

    added check energy decorator, added eat food task

---
+++ Plugins/GameFeatures/DevosYeltoZombie/Source/DevosYeltoZombieRuntime/Private/Dec_CheckEnergy.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Dec_CheckEnergy.h"
+#include "AIController.h"
+#include "Survivor/SurvivorPawn.h"
+#include "Common/StaminaComponent.h"
+
+bool UDec_CheckEnergy::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	ASurvivorPawn* survivor = Cast<ASurvivorPawn>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!survivor) return false;
+	
+	UStaminaComponent* staminaComp = Cast<UStaminaComponent>(survivor->GetComponentByClass(UStaminaComponent::StaticClass()));
+	if (!staminaComp) return false;
+	
+	float staminapercent{ (staminaComp->GetCurrentStamina()) / (staminaComp->GetMaxStamina()) };
+	
+	if (staminapercent <= 0.3)
+	{
+		GEngine->AddOnScreenDebugMessage(5, 2.f, FColor::Red, 
+			FString::Printf(TEXT("Health under threshold")));
+		return true;
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/DevosYeltoZombie/Source/DevosYeltoZombieRuntime/Private/Task_EatFood.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Task_EatFood.h"
+#include "AIController.h"
+#include "Survivor/SurvivorPawn.h"
+#include "Common/InventoryComponent.h"
+
+EBTNodeResult::Type UTask_EatFood::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	ASurvivorPawn* survivor = Cast<ASurvivorPawn>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!survivor) return EBTNodeResult::Failed;
+	
+	UInventoryComponent* InventoryComponent = Cast<UInventoryComponent>(survivor->GetComponentByClass(UInventoryComponent::StaticClass()));
+	if (!InventoryComponent) return EBTNodeResult::Failed;
+	
+	const int foodIdx{3};
+	if (InventoryComponent->UseItem(foodIdx))
+	{
+		if (InventoryComponent->GetInventory()[foodIdx]->GetValue() <=0)
+		{
+			InventoryComponent->RemoveItem(foodIdx);
+		};
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::Failed;
+}
